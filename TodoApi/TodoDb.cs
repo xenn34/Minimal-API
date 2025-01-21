80a197685d78cf33468afacbdb698fb15292ece1
Hoang Huy <nguyenhoanghuy01027778@gmail.com>
2025-01-21 23:59:00 +0700

    first commit Minimal API

---
+++ TodoApi/TodoDb.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApi;
+
+class TodoDb : DbContext
+{
+    public TodoDb(DbContextOptions<TodoDb> options)
+        : base(options) { }
+
+    public DbSet<Todo> Todos => Set<Todo>();
+}
